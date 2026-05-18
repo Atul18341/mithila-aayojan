aa8c5c1438bbc0229792a6fc30366c2d714230aa
Atul18341 <atulkumar987613@gmail.com>
2026-05-18 13:28:18 +0530

    Event Page Module Conversion

---
+++ src/app/events/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+export default function Events() {
+return(
+    <div>
+        Under Development.
+    </div>
+)
+}
